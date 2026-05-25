c5282fd842d09b6e19a037e9ce4d810bab6c891c
AlejandroFV74 <00067323@uca.edu.sv>
2026-05-25 15:34:46 -0600

    feat: database connection

---
+++ EjercicioParcialDos/src/main/java/com/aafv/ejercicioparcialdos/EjercicioParcialDosApplication.java
@@ -0,0 +1,13 @@
+package com.aafv.ejercicioparcialdos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioParcialDosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EjercicioParcialDosApplication.class, args);
+    }
+
+}
---
+++ EjercicioParcialDos/src/test/java/com/aafv/ejercicioparcialdos/EjercicioParcialDosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aafv.ejercicioparcialdos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioParcialDosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
